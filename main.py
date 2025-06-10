79fcfaeeb392d47544ea70575bf8b49ab36a0c48
CodeVoyager-a11 <dqe83057@gmail.com>
2025-06-10 15:48:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-
 import streamlit as st
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
+import matplotlib.pyplot as plt
+
 # 데이터 정의
 data = {
     '연도': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
     '전체': [67.0, 67.5, 67.7, 66.2, 67.7, 67.1, 65.1, 67.7, 69.6, 70.3],
     '남자': [69.0, 69.0, 69.0, 67.8, 69.6, 69.0, 67.1, 69.5, 71.2, 72.4],
-    '여자': [None]*10  # 여학생 데이터가 남학생과 바뀐 것으로 보여 보완 가능
+    '여자': [65.0, 66.0, 66.5, 64.8, 66.2, 65.3, 63.2, 65.8, 67.9, 68.5]  # 예시값
 }
 df = pd.DataFrame(data)
 
